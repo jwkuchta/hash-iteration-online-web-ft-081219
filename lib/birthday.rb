814e0ecc95300b3f790e310cdb8b61aaf4bff55a
Justina Kuchta <justyna.kuchta@gmail.com>
2019-12-08 16:32:49 -0500

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,24 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+birthday_kids = {
+  "Timmy" => 9, 
+  "Sarah" => 6, 
+  "Amanda" => 27
+}
 
-def happy_birthday(birthday_kids)
+def happy_birthday(kids)
   # add your code snippet here!
+  kids.each {|kid, age| puts "Happy Birthday #{kid}! You are now #{age} years old!"}
 end
+
+def age_appropriate_birthday(kids)
+  kids.each do |kid, age|
+    if age > 12
+      puts "You are too old for this."
+    else
+      puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    end
+  end
+end
+
+puts age_appropriate_birthday(birthday_kids)
 
 
